feb69170031a485434538b868cbcd592180a8f0a
humgate <humgate@gmail.com>
2024-08-04 21:47:14 +0500

    trained ViT

--- multi_class_vit/model.py
+++ multi_class_vit/model.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+from multi_class_vit.embedding import Embedding
+
 
 class MultiHeadAttentionBlock(nn.Module):
     """Creates a multi-head attention block ("MHA").
@@ -48,9 +50,9 @@ class MLPBlock(nn.Module):
                       out_features=linear_hidden_units),
             nn.GELU(),  # "The MLP contains two layers with a GELU non-linearity (section 3.1)."
             nn.Dropout(p=dropout),
-            nn.Linear(in_features=linear_hidden_units,  # needs to take same in_features as out_features of layer above
-                      out_features=embedding_dim),  # take back to embedding_dim
-            nn.Dropout(p=dropout)  # "Dropout, when used, is applied after every dense layer."
+            nn.Linear(in_features=linear_hidden_units,
+                      out_features=embedding_dim),
+            nn.Dropout(p=dropout)
         )
 
     def forward(self, x):
@@ -58,3 +60,97 @@ class MLPBlock(nn.Module):
         x = self.mlp(x)
         return x
 
+
+class TransformerEncoderBlock(nn.Module):
+    """Creates a Transformer Encoder block."""
+
+    def __init__(self,
+                 embedding_dim: int = 768,  # d_model
+                 num_heads: int = 12,  # Amount of attention heads
+                 attn_dropout: float = 0,  # % of dropout for attention layers
+                 linear_hidden_units: int = 3072,  # 4 * d_model
+                 linear_dropout: float = 0.1):  # % of dropout for MLP
+        super().__init__()
+
+        # MHA block
+        self.mha_block = MultiHeadAttentionBlock(embedding_dim=embedding_dim,
+                                                 num_heads=num_heads,
+                                                 attn_dropout=attn_dropout)
+
+        # MLP block
+        self.mlp_block = MLPBlock(embedding_dim=embedding_dim,
+                                  linear_hidden_units=linear_hidden_units,
+                                  dropout=linear_dropout)
+
+    def forward(self, x):
+        #  Forward through MHA and apply skip connection (residual connection) for MHA block
+        x = self.mha_block(x) + x
+
+        #  Forward through MLP and apply skip connection (residual connection) for MLP block
+        x = self.mlp_block(x) + x
+
+        return x
+
+
+class ViT(nn.Module):
+    """Creates a Vision Transformer model."""
+
+    def __init__(self,
+                 image_size: int = 224,  # Training resolution of image
+                 num_channels: int = 3,  # Number of channels in input image
+                 patch_size: int = 16,  # Patch size
+                 num_transformer_layers: int = 12,  # Layers from paper for ViT-Base
+                 embedding_dim: int = 768,  # d_model for ViT-Base
+                 linear_hidden_units: int = 3072,  # MLP size for ViT-Base
+                 num_heads: int = 12,  # MHA for ViT-Base
+                 attn_dropout: float = 0,  # Dropout for attention projection
+                 linear_dropout: float = 0.1,  # % of dropout for MLP layers
+                 embedding_dropout: float = 0.1,  # Dropout for patch and position embeddings
+                 num_classes: int = 3,
+                 batch_size: int = 32):
+        super().__init__()
+
+        # Check the image size is divisible by the patch size
+        assert image_size % patch_size == 0, (f"Input image size must be divisible by patch size,"
+                                              f" image shape: {image_size}, "
+                                              f"patch size: {patch_size}")
+
+        # Create learnable embedding
+        self.embedding = Embedding(image_size=image_size,
+                                   num_channels=num_channels,
+                                   patch_size=patch_size,
+                                   embedding_dim=embedding_dim,
+                                   batch_size=batch_size)
+
+        # Create embedding dropout value
+        self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+
+        # Create Transformer Encoder blocks (we can stack Transformer Encoder blocks using nn.Sequential())
+        self.transformer_encoder = nn.Sequential(  # "*" means "all"
+            *[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                      num_heads=num_heads,
+                                      linear_hidden_units=linear_hidden_units,
+                                      linear_dropout=linear_dropout) for _ in
+              range(num_transformer_layers)])
+
+        # Create linear classifier
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.Linear(in_features=embedding_dim,
+                      out_features=num_classes)
+        )
+
+    def forward(self, x):
+        # Create embedding
+        x = self.embedding(x)
+
+        # Run embedding dropout (Appendix B.1)
+        x = self.embedding_dropout(x)
+
+        # Pass embedding through transformer encoder layers
+        x = self.transformer_encoder(x)
+
+        # Put 0 index logit through classifier
+        x = self.classifier(x[:, 0])  # run on each sample in a batch at 0 index
+
+        return x
--- multi_class_vit/model_operator.py
+++ multi_class_vit/model_operator.py
@@ -1,9 +1,15 @@
 import torch
+import torchmetrics
+from torch import nn
 from torch.utils.data import DataLoader
 
 from multi_class_vit.dataset import load_dataset_vit
-from multi_class_vit.embedding import Embedding
-from multi_class_vit.model import MultiHeadAttentionBlock, MLPBlock
+
+from multi_class_vit.model import ViT
+from util.model_functions import train
+from torchinfo import summary
+
+from util.plotter import Plotter
 
 
 def multi_class_vit_model_operator():
@@ -16,28 +22,45 @@ def multi_class_vit_model_operator():
         patch_size=16
     )
 
-    batch_size = 32
+    batch_size = 16
     train_dataloader = DataLoader(dataset=train_data,
                                   batch_size=batch_size,
-                                  shuffle=True)
+                                  shuffle=True,
+                                  drop_last=True)
     test_dataloader = DataLoader(dataset=test_data,
                                  batch_size=batch_size,
-                                 shuffle=False)
+                                 shuffle=False,
+                                 drop_last=True)
 
-    patchify = Embedding(image_size=224,
-                         num_channels=3,
-                         patch_size=16,
-                         embedding_dim=3 * 16 * 16,
-                         batch_size=1)
+    vit_model = ViT(image_size=224,
+                    num_channels=3,
+                    patch_size=16,
+                    embedding_dim=3 * 16 * 16,
+                    linear_hidden_units=4 * 768,
+                    num_heads=12,
+                    linear_dropout=0.1,
+                    embedding_dropout=0.1,
+                    num_classes=3,
+                    batch_size=batch_size).to(device)
 
-    image_batch, label_batch = next(iter(train_dataloader))
-    image = image_batch[0]  # torch.Size([1, 3, 224, 224])
-    embedding = patchify(image.unsqueeze(0))
-    multi_head_attn_block = MultiHeadAttentionBlock(embedding_dim=768,
-                                                    num_heads=12,
-                                                    attn_dropout=0)
-    attn = multi_head_attn_block(embedding)
-    mlp_block = MLPBlock(embedding_dim=768, linear_hidden_units=3072, dropout=0.1)
-    mlp_output = mlp_block(attn)
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    optimizer = torch.optim.Adam(params=vit_model.parameters(),
+                                 lr=3e-3,  # Base LR from for ViT from paper
+                                 betas=(0.9, 0.999),  # default values and also mentioned in ViT paper section 4.1
+                                 weight_decay=0.3)  # from the ViT paper section 4.1 (Training & Fine-tuning)
 
-    print("done")
+    summary(vit_model, input_size=(batch_size, 3, 224, 224))
+
+    vit_model_results = train(model=vit_model,
+                              train_data_loader=train_dataloader,
+                              test_data_loader=test_dataloader,
+                              loss_fn=nn.CrossEntropyLoss().to(device),
+                              optimizer=optimizer,
+                              accuracy_fn=torchmetrics.Accuracy("multiclass", num_classes=3).to(device),
+                              epochs=5,
+                              device=torch.device(device))
+
+    Plotter.plot_loss_accuracy_curves(vit_model_results)
+
+
